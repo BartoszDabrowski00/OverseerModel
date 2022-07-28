758f82e02d48a6fb805c0d1f72c19914714b81fd
Bartek <bartosz.dabrowski2000@gmail.com>
2022-07-28 20:31:10 +0200

    More logs, fstrings and new recordings

--- emotoins_printer.py
+++ emotoins_printer.py
@@ -2,7 +2,7 @@ from typing import Dict
 
 
 def print_all_emotions_distribution(file_path: str, emotions_distribution: Dict[str, float]) -> None:
-    print("File:" + file_path)
+    print(f'File:{file_path}')
     for emotion_class, value in emotions_distribution.items():
-        print("\t" + emotion_class + ": " + str(value))
-
+        print(f'\t{emotion_class}: {value}')
+    print()
