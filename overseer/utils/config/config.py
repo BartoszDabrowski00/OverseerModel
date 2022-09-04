e94b1e0cf9c3b543e3f781ea5011fadc20c82fda
Bartek <bartosz.dabrowski2000@gmail.com>
2022-09-04 19:35:57 +0200

    Refactor project structure and prepare to use as queue worker

---
+++ overseer/utils/config/config.py
@@ -0,0 +1,15 @@
+import os
+
+import toml
+
+
+class Config:
+    CONFIG_PATH = os.getenv('OVERSEER_MODEL_CONFIG_PATH', 'config.toml')
+    config = None
+
+    @classmethod
+    def get(cls, section, key):
+        if cls.config is None:
+            cls.config = toml.load(cls.CONFIG_PATH)
+
+        return cls.config.get(section, {}).get(key, None)
